fbcea89072f1a84e0b5b85c00840a11f464b6eff
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-08-06 01:22:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  Handlebars.registerHelper('display_ingredient', function(ingredient) {
+  	return new Handlebars.SafeString("<li>" + ingredient + "</li>")})
+
+  var formTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+  document.getElementsByTagName("main")[0].innerHTML += formTemplate({'action': 'createRecipe()'});
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function createRecipe() {
+	var recipeTemplate = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+	document.getElementsByTagName("main")[0].innerHTML += recipeTemplate; 
+}
+
+function displayEditForm() {
+	var formTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+	document.getElementsByTagName("main")[0].innerHTML += formTemplate;
+}
+
+function updateRecipe() {
+
+}
